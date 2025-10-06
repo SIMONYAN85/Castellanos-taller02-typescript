e3c65e421c32284f74128d88b200aebd1cd80c2d
Dayan <dnan@gmail.com>
2025-10-05 20:04:35 -0500

    feat: ejecución del sistema de empleados con instancia de BaseEmployee

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
 console.log('Sistema de empleados iniciado correctamente.');
+import { BaseEmployee } from "./classes/BaseEmployee";
+import { Department } from "./interfaces/types";
+
+const empleado = new BaseEmployee(
+  1,
+  "jeniffer",
+  45,
+  "daymoon7@gmail.com",
+  "Femenino",
+  "Desarrolladora",
+  Department.IT,
+  5000
+);
+
+console.log(empleado.getSummary());
