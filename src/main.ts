f3bee20b213ee156c4543a54fe56c6e1e7ffeb36
Dayan <dnan@gmail.com>
2025-10-05 20:26:56 -0500

    feat: Lista de empleados y visualización en consola

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
-console.log('Sistema de empleados iniciado correctamente.');
 import { BaseEmployee } from "./classes/BaseEmployee";
 import { Department } from "./interfaces/types";
 
-const empleado = new BaseEmployee(
-  1,
-  "jeniffer",
-  45,
-  "daymoon7@gmail.com",
-  "Femenino",
-  "Desarrolladora",
-  Department.IT,
-  5000
-);
+console.log("Sistema de empleados iniciado correctamente.");
 
-console.log(empleado.getSummary());
+//Lista de empleados
+const empleados: BaseEmployee[] = [
+  new BaseEmployee(1, "Jeniffer", 45, "jenaP@gmail.com", "Femenino", "Desarrolladora", Department.IT, 5000),
+  new BaseEmployee(2, "Josefa", 32, "josefinanda@gmail.com", "Femenino", "Analista de datos", Department.MARKETING, 4000),
+  new BaseEmployee(3, "Juan", 28, "ana@example.com", "Masculino", "RRHH", Department.HR, 3500)
+];
+
+// 🧪 Visualización en consola
+empleados.forEach(e => {
+  console.log(e.getSummary());
+});
